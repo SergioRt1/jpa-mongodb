782ae7479816d0987d7c2a04c874a91f55dc3e0f
SergioRt1 <sergio200035@gmail.com>
2019-03-30 00:14:38 -0500

    Part 1: Basic Mongo DB configuration and Spring Boot Integration

--- src/main/java/eci/cosw/Application.java
+++ src/main/java/eci/cosw/Application.java
@@ -1,7 +1,9 @@
 package eci.cosw;
 
 import eci.cosw.data.CustomerRepository;
+import eci.cosw.data.TodoRepository;
 import eci.cosw.data.model.Customer;
+import eci.cosw.data.model.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,13 +16,15 @@ public class Application implements CommandLineRunner {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private TodoRepository todoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         customerRepository.deleteAll();
 
@@ -35,8 +39,20 @@ public class Application implements CommandLineRunner {
         for (Customer customer : customerRepository.findAll()) {
             System.out.println(customer);
         }
-        System.out.println();
+        System.out.println("-------------------------------");
 
+        todoRepository.deleteAll();
+
+        todoRepository.save(new Todo("travel to Galapagos",10,"Jan 10 - 1860","charles@natural.com","pending"));
+        todoRepository.save(new Todo("Pass IETI",10,"Jan 10 - 2019","sergioRt@natural.com","done"));
+        todoRepository.save(new Todo("travel to tokyo",2,"Jan 20 - 1900","charles@natural.com","pending"));
+
+        System.out.println("Todos found with findByResponsible():");
+        System.out.println("-------------------------------");
+        for (Todo todo : todoRepository.findByResponsible("charles@natural.com")) {
+            System.out.println(todo);
+        }
+        System.out.println("-------------------------------\n");
 
     }
 
---
+++ src/main/java/eci/cosw/data/TodoRepository.java
@@ -0,0 +1,16 @@
+package eci.cosw.data;
+
+import eci.cosw.data.model.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+    List<Todo> findByResponsible(String responsible);
+
+    List<Todo> findByPriority(Integer responsible);
+
+    List<Todo> findAllByStatus(String status);
+
+}
